c632c86672f9b22de312b9d911a64782b2d839bc
Jurredr <github@jurre.me>
2021-06-06 14:04:46 +0200

    Basic express routes

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,21 @@
+require("dotenv").config();
 import { Request, Response } from "express"
 
+// Setup express
 const express = require('express')
 const app = express()
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+// Handle skill-box api request
+app.get('/api/', (req: Request, res: Response) => {
+    const test = req.query.languages
+    res.send('Languages: ' + test)
 })
 
-app.listen(3000, () => {
-    console.log(`Example app listening at http://localhost:3000`)
+// Send 404 for incorrect request URL
+app.use('*', (req: Request, res: Response) => {
+    res.sendStatus(404);
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
 })
