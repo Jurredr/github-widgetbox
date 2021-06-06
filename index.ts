65a45625ebba5faffab1394c33552cf6c1237059
Jurredr <github@jurre.me>
2021-06-06 12:43:05 +0200

    Some default config files & index.ts

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log(`Example app listening at http://localhost:3000`);
+});
