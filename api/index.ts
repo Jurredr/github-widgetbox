a017b59406c4ad9090dbce767628ecaef44c07be
Jurredr <github@jurre.me>
2021-06-06 14:22:23 +0200

    Basic query parameter parsing

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
-require("dotenv").config();
-import { Request, Response } from "express"
+require('dotenv').config()
+import { Request, Response } from 'express'
 
 // Setup express
 const express = require('express')
@@ -7,14 +7,28 @@ const app = express()
 
 // Handle skill-box api request
 app.get('/api/', (req: Request, res: Response) => {
-    const test = req.query.languages
-    res.send('Languages: ' + test)
+    // Get the query parameters
+    const { username, languages, tools, } = req.query
+
+    // Content response-type will be SVG
+    // res.setHeader("Content-Type", "image/svg+xml");
+
+    // Fetch stats here
+
+    res.send(
+        'Username: ' +
+            username +
+            '<br> Languages: ' +
+            languages +
+            '<br> Tools: ' +
+            tools
+    )
 })
 
 // Send 404 for incorrect request URL
 app.use('*', (req: Request, res: Response) => {
-    res.sendStatus(404);
-});
+    res.sendStatus(404)
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Example app listening at http://localhost:${process.env.PORT}`)
