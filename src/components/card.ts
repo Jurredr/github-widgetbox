552354ce83e12c13856f0860c69ccba7ea104963
Jurredr <github@jurre.me>
2021-06-11 19:33:13 +0200

    Card component added

---
+++ src/components/card.ts
@@ -0,0 +1,18 @@
+export default function buildCard(
+    width: number,
+    height: number,
+    bg: string
+): string {
+    return `<defs>
+                <filter id="Card" x="0" y="0" width="${width}" height="${height}" filterUnits="userSpaceOnUse">
+                    <feOffset dy="3" input="SourceAlpha"/>
+                    <feGaussianBlur stdDeviation="5" result="blur"/>
+                    <feFlood flood-opacity="0.161"/>
+                    <feComposite operator="in" in2="blur"/>
+                    <feComposite in="SourceGraphic"/>
+                </filter>
+            </defs>
+            <g id="card" transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Card)">
+                <rect id="Card" data-name="Card" width="${width}" height="${height}" rx="30" transform="translate(15 12)" fill="${bg}"/>
+            </g>`
+}
