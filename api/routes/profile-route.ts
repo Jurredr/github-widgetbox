dcbe26a3251d0f1bcc90518ef137dee4a9e23805
Jurredr <github@jurre.me>
2021-06-18 22:34:02 +0200

    Username validity checking with RegEx, added comments

--- api/routes/profile-route.ts
+++ api/routes/profile-route.ts
@@ -5,20 +5,40 @@ const router = express.Router()
 
 import profileWidget from '../../src/widgets/profile'
 import errorWidget from '../../src/widgets/error'
+const githubUsernameRegex = require("github-username-regex")
 
 // Primary profile route
 router.get('/', function (req: Request, res: Response) {
     const { username } = req.query
 
+    // Set the header's type to svg/xml
     res.setHeader('Content-Type', 'image/svg+xml')
 
-    if (username === undefined) {
-        res.send(errorWidget('Profile', '-25%', 'Username is undefined!', '-26%'))
+    // Check if argument is not present
+    if (username === undefined || username === null) {
+        res.send(
+            errorWidget('Profile', '-25%', 'Username is undefined!', '-26%')
+        )
         return
     }
 
-    profileWidget(String(username)).then(response => {
-        res.send(response)
+    // Check validity based on GitHub's username rules
+    if (!githubUsernameRegex.test(username)) {
+        res.send(
+            errorWidget('Profile', '-25%', 'Username is invalid!', '-22%')
+        )
+        return
+    }
+
+    // Grab the Profile widget
+    profileWidget(String(username)).then((response) => {
+        if (response === undefined || response === null) {
+            res.send(
+                errorWidget('Profile', '-25%', 'GitHub API-call error!', '-24%')
+            )
+        } else {
+            res.send(response)
+        }
     })
 })
 
