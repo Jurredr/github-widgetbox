2c8b13465a430dea4e82edfc993fe39239c07abe
Jurredr <github@jurre.me>
2021-06-18 21:10:06 +0200

    User profile grabbing through GitHub API

--- api/routes/profile-route.ts
+++ api/routes/profile-route.ts
@@ -4,12 +4,19 @@ import express from 'express'
 const router = express.Router()
 
 import profileWidget from '../../src/widgets/profile'
+import errorWidget from '../../src/widgets/error'
 
 // Primary profile route
 router.get('/', function (req: Request, res: Response) {
     const { username } = req.query
 
     res.setHeader('Content-Type', 'image/svg+xml')
+
+    if (username === undefined) {
+        res.send(errorWidget('Username is not defined!'))
+        return
+    }
+
     res.send(profileWidget(String(username)))
 })
 
--- src/widgets/profile.ts
+++ src/widgets/profile.ts
@@ -1,3 +1,16 @@
+import axios from 'axios'
+
 export default function profileWidget(username: string): string {
-    return 'test'
+
+    try {
+        const profile = axios.get('https://api.github.com/users/' + username)
+
+        Promise.all([profile]).then((results) => {
+            console.log(results[0].data)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+
+    return `<svg></svg>`
 }
