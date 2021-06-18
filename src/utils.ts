37e1dbe1800585909b2eb0d8a6f4acc570d189e6
Jurredr <github@jurre.me>
2021-06-18 23:17:08 +0200

    Added helper function to fetch Base64 string from GET request

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 export function isValidHexColor(hexColor: string): boolean {
     return new RegExp(
         /^([A-Fa-f0-9]{8}|[A-Fa-f0-9]{6}|[A-Fa-f0-9]{3}|[A-Fa-f0-9]{4})$/
@@ -6,4 +8,12 @@ export function isValidHexColor(hexColor: string): boolean {
 
 export function getBoolean(str: string): boolean {
     return str.toLowerCase() === 'true'
+}
+
+export async function requestInBase64(url: string): Promise<string> {
+    const response = await axios
+        .get(url, {
+            responseType: 'arraybuffer',
+        })
+    return Buffer.from(response.data, 'binary').toString('base64')
 }
