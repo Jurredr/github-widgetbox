80d7fd79e45872ca13578102aaadf604a706f614
Jurredr <github@jurre.me>
2021-06-18 21:10:58 +0200

    Removed redundant githubRequest function

--- src/utils.ts
+++ src/utils.ts
@@ -7,13 +7,3 @@ export function isValidHexColor(hexColor: string): boolean {
 export function getBoolean(str: string): boolean {
     return str.toLowerCase() === 'true'
 }
-
-// TODO: nice implementation of util for GitHub API requests
-// export function githubRequest(method: string, dest: string, data: string, headers: string) {
-//     return axios({
-//         url: 'https://api.github.com/' + dest,
-//         method,
-//         headers,
-//         data,
-//     })
-// }
