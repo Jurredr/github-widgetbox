0e7fd84356ba99d9cc655e4cd51d9d9b96fe628e
Jurredr <github@jurre.me>
2021-09-21 18:12:09 +0200

    Added VueJS framework data

---
+++ src/data/frameworks.ts
@@ -0,0 +1,14 @@
+const frameworks = [
+    {
+        name: ['Vue', 'VueJS'],
+        colorFrom: '#AFF8D6',
+        colorTo: '#00B86B',
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" width="31.5" height="26.979" viewBox="0 0 31.5 26.979">
+        <path id="vue-logo" data-name="vue-logo" d="M25.095,4.521H19.688l-3.937,6.23-3.375-6.23H0L15.75,31.5,31.5,4.521ZM3.916,6.771H7.7L15.75,20.707,23.794,6.771h3.783L15.75,27.035,3.916,6.771Z" transform="translate(0 -4.521)" fill="#fff"/>
+        </svg>`,
+        width: 31.5,
+        height: 26.979,
+    },
+]
+
+export default frameworks
