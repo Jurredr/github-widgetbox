3a4831c9be34d164d6c0ecb4e008c14a8be63495
Jurredr <github@jurre.me>
2021-09-25 11:42:57 +0200

    Added Tools data file & added Git tool data

---
+++ src/data/tools.ts
@@ -0,0 +1,18 @@
+import { IconData } from '../interfaces/IconData'
+
+const tools: IconData[] = [
+    {
+        name: ['Git'],
+        colorFrom: '#FFC1B5',
+        colorTo: '#F05033',
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 36 36">
+        <path id="git-logo" data-name="git-logo" d="M35.319,16.4,19.6.678a2.324,2.324,0,0,0-3.282,0L13.062,3.941,17.2,8.08a2.757,2.757,0,0,1,3.49,3.511l3.987,3.99a2.756,2.756,0,1,1-1.656,1.565L19.29,13.432V23.22a2.761,2.761,0,1,1-3.181.525,2.826,2.826,0,0,1,.907-.609V13.252a2.761,2.761,0,0,1-1.494-3.615L11.454,5.55.675,16.322a2.334,2.334,0,0,0,0,3.284L16.4,35.321a2.318,2.318,0,0,0,3.279,0L35.319,19.676a2.317,2.317,0,0,0,0-3.281" transform="translate(0 0)" fill="#fff"/>
+        </svg>
+        `,
+        width: 36,
+        height: 36,
+        xOffset: 0,
+    },
+]
+
+export default tools
