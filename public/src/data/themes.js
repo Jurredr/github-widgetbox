9294835bf236d94752651233b786c8ecf3a1b334
Jurredr <github@jurre.me>
2022-10-23 16:11:26 +0100

    Rebuilt the project

--- public/src/data/themes.js
+++ public/src/data/themes.js
@@ -3,14 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const themes = [
     {
         name: ['dark', 'darkmode'],
-        background: '#18191A',
-        title: '#E4E6EB'
+        background: '#201D2A',
+        title: '#E4E6EB',
     },
     {
         name: ['default', 'light'],
         background: '#FFFFFF',
-        title: '#000000'
-    }
+        title: '#000000',
+    },
 ];
 exports.default = themes;
 //# sourceMappingURL=themes.js.map
